ff91069f331725db7d0ea002539a9c68cebe2704
Sai Sathvik Avva <saisathvik.avva80@gmail.com>
2026-06-26 18:59:19 -0400

    add getCoral() Method for Wrist Angle

--- ik-2027/src/main/java/frc/robot/RobotContainer.java
+++ ik-2027/src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,37 @@
 
 package frc.robot;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.WristSubsystem;
+import frc.robot.utils.InverseKinematicsMath;
+
+
 
 public class RobotContainer {
+  WristSubsystem wristSubsystem = WristSubsystem.getInstance();
+
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+public Command getCoral(){
+  Angle wristAngle = InverseKinematicsMath.calculateWristAngle(0.5, 0.5);
+
+  return Commands.sequence(
+  //void method conversion into command since sequence takes commands as parameters
+    Commands.runOnce(() -> wristSubsystem.setWristSetpoint(wristAngle), wristSubsystem),
+    
+    Commands.waitSeconds(1)
+  );
+}
+
+  private void configureBindings() {
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
